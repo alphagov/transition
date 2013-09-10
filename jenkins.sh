c5e8160c81cffade6ebcfb653f29aab59d7430f4
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-09-10 15:59:47 +0100

    Put jenkins.sh back

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+export RAILS_ENV=test
+export DISPLAY=":99"
+
+bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+bundle exec rake db:drop db:create db:schema:load
+bundle exec rake assets:clean assets:precompile
+bundle exec rake
