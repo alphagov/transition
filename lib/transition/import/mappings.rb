12e73c02ccfde97174ac56061b91227c87561b05
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-09-16 09:00:49 +0100

    Import/update suggested and archive URLs if present

--- lib/transition/import/mappings.rb
+++ lib/transition/import/mappings.rb
@@ -12,7 +12,7 @@ module Transition
         FIELDS TERMINATED BY ','
         LINES TERMINATED BY '\\n'
         IGNORE 1 LINES
-          (@old_url, new_url, http_status)
+          (@old_url, new_url, http_status, suggested_url, archive_url)
         SET
           host = TRIM(LEADING 'http://' FROM SUBSTRING_INDEX(@old_url, '/', 3)), /* Everything up to the 3rd slash or end of string */
           path = replace(@old_url, SUBSTRING_INDEX(@old_url, '/', 3), ''),       /* wow, this is ugly */
@@ -21,12 +21,13 @@ module Transition
       mySQL
 
       INSERT_FROM_STAGING = <<-mySQL
-        INSERT INTO mappings (site_id, path, path_hash, http_status, new_url)
-        SELECT h.site_id, st.path, st.path_hash, st.http_status, new_url
+        INSERT INTO mappings (site_id, path, path_hash, http_status, new_url, suggested_url, archive_url)
+        SELECT h.site_id, st.path, st.path_hash, st.http_status, st.new_url, st.suggested_url, st.archive_url
         FROM
           mappings_staging st
         INNER JOIN hosts h on h.hostname = st.host
-        ON DUPLICATE KEY UPDATE http_status = st.http_status, new_url = st.new_url
+        ON DUPLICATE KEY UPDATE http_status = st.http_status, new_url = st.new_url,
+                                suggested_url = st.suggested_url, archive_url = st.archive_url
       mySQL
 
       def self.from_redirector_csv_file!(filename)
--- spec/lib/transition/import/mappings_spec.rb
+++ spec/lib/transition/import/mappings_spec.rb
@@ -6,13 +6,13 @@ describe Transition::Import::Mappings do
     @ago_site = create :site, abbr: 'ago' do |site|
       site.hosts << create(:host, hostname: 'www.ago.gov.uk')
     end
-    @decc_site = create :site, abbr: 'decc' do |site|
-      site.hosts << create(:host, hostname: 'www.decc.gov.uk')
+    @directgov_site = create :site, abbr: 'dg' do |site|
+      site.hosts << create(:host, hostname: 'www.direct.gov.uk')
     end
   end
 
   describe '.from_redirector_csv_file!' do
-    context 'a single import', testing_before_all: true do
+    context 'a single import from a file with no suggested/archive URLs', testing_before_all: true do
       before :all do
         create_test_sites
         Transition::Import::Mappings.from_redirector_csv_file!('spec/fixtures/mappings/ago_abridged.csv')
@@ -31,19 +31,23 @@ describe Transition::Import::Mappings do
       end
     end
 
-    context 'an import with updates', testing_before_all: true do
+    context 'an import with updates to a file with suggested/archive URLs', testing_before_all: true do
       before :all do
         create_test_sites
-        Transition::Import::Mappings.from_redirector_csv_file!('spec/fixtures/mappings/ago_abridged.csv')
-        Transition::Import::Mappings.from_redirector_csv_file!('spec/fixtures/mappings/updated/ago_updated.csv')
+        Transition::Import::Mappings.from_redirector_csv_file!('spec/fixtures/mappings/directgov_abridged.csv')
+        Transition::Import::Mappings.from_redirector_csv_file!('spec/fixtures/mappings/updated/directgov_updated.csv')
       end
 
       it 'has registered the new mapping' do
-        Mapping.count.should == 4
+        Mapping.find_by_path('/new-mapping').should_not be_nil
       end
 
-      it 'has updated the old mapping with a new_url' do
-        @ago_site.mappings.where(path: '/_layouts/feed.aspx').first.new_url.should == 'https://www.gov.uk/updated-url'
+      describe 'the update of the old mapping' do
+        subject(:updated_mapping) { @directgov_site.mappings.where(path: '/barrierbusting').first }
+
+        its(:new_url)       { should eql('http://new.url') }
+        its(:suggested_url) { should include('barrierbusting.updated') }
+        its(:archive_url)   { should include('webarchive.updated') }
       end
     end
   end
@@ -56,6 +60,12 @@ describe Transition::Import::Mappings do
 
     it 'imported mappings from both files' do
       Mapping.count.should == 6
+    end
+
+    describe 'the directgov mapping with suggested and archive urls' do
+      subject { Mapping.find_by_path!('/barrierbusting') }
+      its(:suggested_url) { should eql('http://barrierbusting.communities.gov.uk/') }
+      its(:archive_url)   { should eql('http://webarchive.nationalarchives.gov.uk/20121015000000/www.direct.gov.uk/en/Nl1/Newsroom/DG_192969')}
     end
   end
 
