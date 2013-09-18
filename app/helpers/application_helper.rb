9c7d1be2971aa1388133348f37fbcdd5dea76f1b
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-09-18 09:12:58 +0100

    Add a breadcrumb-type nav that isn't really a breadcrumb (it's navigation)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  ##
+  # Render a bootstrap style-compatible breadcrumb. Knows about our models.
+  # Pass in the current significant model to generate, or nothing if you're
+  # at the top level.
+  def breadcrumb(model = nil)
+    content_tag(:ul, nil, class: 'breadcrumb') do
+      crumb_li('Organisations', organisations_path, model == nil) + list_items_for(model)
+    end
+  end
+
+  def list_items_for(model, active = true)
+    case model
+    when Organisation
+      crumb_li(model.title, organisation_path(model), active)
+    when Site
+      list_items_for(model.organisation, false) + crumb_li("#{model.abbr} Mappings", site_mappings_path(model), active)
+    when Mapping
+      list_items_for(model.site, false) + crumb_li('Mapping', '#{foobar}', true)
+    end
+  end
+
+  def crumb_li(title, path, active)
+    options = {}
+    options[:class] = 'active' if active
+
+    content_tag :li, nil, options do
+      active ? title : link_to(title, path) + content_tag(:span, nil, class: 'divider' )
+    end
+  end
 end
