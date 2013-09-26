01e3e8f279d2e3d89cc7bbcd2e357774b690504e
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-09-26 17:10:49 +0100

    Switch to db:structure:load for Jenkins
    (we don't have a db/schema.rb any more)

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,6 @@ export RAILS_ENV=test
 export DISPLAY=":99"
 
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:drop db:create db:schema:load
+bundle exec rake db:drop db:create db:structure:load
 bundle exec rake assets:clean assets:precompile
 bundle exec rake
