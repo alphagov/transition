84884188d7f7b41bdb27997e79f48eacc436ad35
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-10-02 16:23:51 +0000

    Make data loading more consistent and complete
    We don't display hit data anywhere yet, but we're starting to look at it.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 
 bundle install
 bundle exec rake db:create db:structure:load
+bundle exec rake db:seed
 
 if [ ! -d data/redirector ]; then
   mkdir -p data && git clone git@github.com:alphagov/redirector data/redirector
@@ -12,4 +13,11 @@ else
   cd ../..
 fi
 
-bundle exec rake db:seed
+if [ ! -d data/transition-stats ]; then
+  mkdir -p data && git clone git@github.com:alphagov/transition-stats data/transition-stats
+else
+  cd data/transition-stats && git pull
+  cd ../..
+fi
+
+bundle exec rake db:import:all
