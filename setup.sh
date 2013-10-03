2bd540504c35712faac0cf3385adc579cbb1e79c
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-10-03 10:24:48 +0000

    Roll up rake tasks into one for speed
    Each invocation of Rake loads the Rails env, so this saves ~30seconds.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,4 @@
 # Setup the app from scratch and populate the database.
 
 bundle install
-bundle exec rake db:create db:structure:load
-bundle exec rake db:seed
-
-bundle exec rake notmodules:sync
-bundle exec rake import:all
+bundle exec rake db:create db:structure:load db:seed notmodules:sync import:all
