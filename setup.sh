e5d225d7000ccd3c5183e07308168872b0431735
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-10-03 09:44:53 +0000

    Switch to a rake task for the git cloning

--- setup.sh
+++ setup.sh
@@ -6,18 +6,5 @@ bundle install
 bundle exec rake db:create db:structure:load
 bundle exec rake db:seed
 
-if [ ! -d data/redirector ]; then
-  mkdir -p data && git clone git@github.com:alphagov/redirector data/redirector
-else
-  cd data/redirector && git pull
-  cd ../..
-fi
-
-if [ ! -d data/transition-stats ]; then
-  mkdir -p data && git clone git@github.com:alphagov/transition-stats data/transition-stats
-else
-  cd data/transition-stats && git pull
-  cd ../..
-fi
-
+bundle exec rake notmodules:sync
 bundle exec rake db:import:all
