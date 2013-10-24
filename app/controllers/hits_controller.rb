eed246dc8758879a6b4579f73d943fbb84ce6c87
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2013-10-24 17:52:26 +0100

    Add simple hits graph to top of hits views
    * Draw graphs using Google Charts API
    * Manipulate hits data into format needed by API, using
    graph_points_from_hits
    * Create hits.js for loading and drawing graph
    * Pass points data through to hits partial

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -7,7 +7,7 @@ class HitsController < ApplicationController
 
   def index
     @hits   = @aggregated_hits.by_path_and_status.page(params[:page]).order('count DESC')
-    @points = @aggregated_hits.by_date
+    @points = graph_points_from_hits "All hits", @aggregated_hits.by_date
   end
 
   def summary
@@ -28,22 +28,35 @@ class HitsController < ApplicationController
 
   def errors
     @hits   = @aggregated_hits.by_path_and_status.errors.page(params[:page]).order('count DESC')
-    @points = @aggregated_hits.by_date_and_status.errors
+    @points = graph_points_from_hits "Errors", @aggregated_hits.by_date_and_status.errors
   end
 
   def archives
     @hits   = @aggregated_hits.by_path_and_status.archives.page(params[:page]).order('count DESC')
-    @points = @aggregated_hits.by_date_and_status.archives
+    @points = graph_points_from_hits "Archives", @aggregated_hits.by_date_and_status.archives
   end
 
   def redirects
     @hits   = @aggregated_hits.by_path_and_status.redirects.page(params[:page]).order('count DESC')
-    @points = @aggregated_hits.by_date_and_status.redirects
+    @points = graph_points_from_hits "Redirects", @aggregated_hits.by_date_and_status.redirects
   end
 
   def other
     @hits   = @aggregated_hits.by_path_and_status.other.page(params[:page]).order('count DESC')
-    @points = @aggregated_hits.by_date_and_status.other
+    @points = graph_points_from_hits "Other", @aggregated_hits.by_date_and_status.other
+  end
+
+  protected
+
+  # Creating format required by
+  # https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart
+  def graph_points_from_hits title, hits
+    points = [["Date", title]]
+    hits.each do |hit|
+      points.push [hit.hit_on.to_s('yyyy-mm-dd'), hit.count]
+    end
+
+    return points
   end
 
 end
