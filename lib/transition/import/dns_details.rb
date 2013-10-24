fe09485e833255aaeb82d6f53c3a0991182b261c
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-10-24 12:49:03 +0100

    Add Transition::Import::DnsDetails
    Augments Host models with real DNS info.

---
+++ lib/transition/import/dns_details.rb
@@ -0,0 +1,36 @@
+require 'resolv'
+
+module Transition
+  module Import
+    ##
+    # Augments Host models with real DNS info
+    class DnsDetails
+      NAMESERVERS = ['8.8.8.8', '8.8.4.4']
+
+      attr_accessor :hosts, :resolver
+
+      def initialize(hosts)
+        self.resolver = Resolv::DNS.new(nameserver: NAMESERVERS)
+        self.hosts = hosts
+      end
+
+      def import!
+        hosts.each do |host|
+          $stderr.print '.'
+          begin
+            cname_record = resolver.getresource(host.hostname, Resolv::DNS::Resource::IN::CNAME)
+          rescue Resolv::ResolvError
+            next
+          end
+          host.cname = cname_record.name.to_s
+          host.ttl   = cname_record.ttl
+          host.save!
+        end
+      end
+
+      def self.from_nameserver!(hosts = Host.all)
+        DnsDetails.new(hosts).import!
+      end
+    end
+  end
+end
