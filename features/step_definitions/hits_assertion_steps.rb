ef5c6bd2f97bc2a0f985a3e6c1946136abea1807
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2013-10-25 13:37:32 +0100

    Break up feature assertions and interactions
    * Split interactions and assertions into feature specific files, eg
    mappings and hits
    * Break up and group generic assertions using comments

---
+++ features/step_definitions/hits_assertion_steps.rb
@@ -0,0 +1,121 @@
+Then(/^I should see all hits for the Attorney General's office in descending count order$/) do
+  within '.hits' do
+    counts = page.all(:css, 'td.count').map { |node| node.text.to_i }
+
+    expect(counts).to be_sorted.descending
+  end
+end
+
+Then(/^I should not see hits for the Cabinet Office site$/) do
+  within '.hits' do
+    expect(page).to_not have_content('/cabinetoffice')
+  end
+end
+
+Then(/^the hits should be grouped by path and status$/) do
+  within '.hits' do
+    expect(page).to have_selector('tbody tr', count: 4)
+    expect(page).to have_selector('tbody tr:first-child .count', text: '300')
+  end
+end
+
+Then(/^the top hit should be represented by a 100% bar$/) do
+  within '.hits' do
+    expect(page).to have_selector('tbody tr:first-child .bar-chart-row[style*="width: 100"]')
+  end
+end
+
+Then(/^subsequent hits should have smaller bars$/) do
+  within '.hits' do
+    expect(page).to have_selector('tbody tr:nth-child(2) .bar-chart-row[style*="width: 66.6"]')
+  end
+end
+
+Then(/^each path should be a link to the real URL$/) do
+  within '.hits' do
+    anchors = page.all(:css, '.path a')
+    expect(anchors).to have(4).links
+  end
+end
+
+Then(/^I should see a section for the most common errors on the Attorney General's office$/) do
+  expect(page).to have_selector('h2', text: 'Errors')
+end
+
+Then(/^I should see a section for the most common archives$/) do
+  expect(page).to have_selector('h2', text: 'Archives')
+end
+
+Then(/^I should see a section for the most common redirects$/) do
+  expect(page).to have_selector('h2', text: 'Redirects')
+end
+
+Then(/^I should see a section for the other hits, the most common miscellany$/) do
+  expect(page).to have_selector('h2', text: 'Other')
+end
+
+Then(/^it should show only the top ten errors in descending count order$/) do
+  within '.hits-errors' do
+    expect(page).to have_sorted_bar_rows(10).for_status(404)
+  end
+end
+
+Then(/^it should show only the top ten archives in descending count order$/) do
+  within '.hits-archives' do
+    expect(page).to have_sorted_bar_rows(10).for_status(410)
+  end
+end
+
+Then(/^it should show only the top ten redirects in descending count order$/) do
+  within '.hits-redirects' do
+    expect(page).to have_sorted_bar_rows(10).for_status(301)
+  end
+end
+
+Then(/^it should show only the top ten other hits in descending count order$/) do
+  within '.hits-other' do
+    expect(page).to have_sorted_bar_rows(10).for_status(200)
+  end
+end
+
+Then(/^I should see all hits with an error status for the Attorney General's office in descending count order$/) do
+  within '.hits' do
+    expect(page).to have_sorted_bar_rows(11).for_status(404)
+  end
+end
+
+Then(/^I should see all hits with an archive status for the Attorney General's office in descending count order$/) do
+  within '.hits' do
+    expect(page).to have_sorted_bar_rows(11).for_status(410)
+  end
+end
+
+Then(/^I should see all hits with a redirect status for the Attorney General's office in descending count order$/) do
+  within '.hits' do
+    expect(page).to have_sorted_bar_rows(11).for_status(301)
+  end
+end
+
+Then(/^I should see all hits with long tail statuses for the Attorney General's office in descending count order$/) do
+  within '.hits' do
+    expect(page).to have_sorted_bar_rows(11).for_status(200)
+  end
+end
+
+Then(/^each hit should have a link to check its mapping$/) do
+  within '.hits tbody' do
+    page.all('tr').each do |row|
+      path = row.find(:css, '.path').text
+      mapping = row.find(:css, '.action')
+      expect(mapping).to have_link('', href: site_mapping_find_path(@site, path: path))
+    end
+  end
+end
+
+Then(/^I should be on the new mapping page$/) do
+  step 'I should see "New mapping"'
+end
+
+Then(/^the top hit's canonicalized path should already be in the form$/) do
+  expect(find_field('Path').value).to eql('/a')
+end
---
+++ features/step_definitions/organisation_assertion_steps.rb
@@ -0,0 +1,15 @@
+Then(/^I should see links to all this organisation's sites and homepages$/) do
+  @organisation.sites.each do |site|
+    expect(page).to have_link('', href: site_mappings_path(site.abbr))
+    expect(page).to have_link('', href: site.homepage)
+  end
+end
+
+Then(/^I should see that this organisation is an executive non-departmental public body of its parent$/) do
+  expect(page).to have_content('is an executive non-departmental public body of')
+  expect(page).to have_link('', href: organisation_path(@organisation.parent))
+end
+
+Then(/^I should see a link to the organisation (.*)$/) do |org_abbr|
+  expect(page).to have_link('', href: organisation_path(org_abbr))
+end
