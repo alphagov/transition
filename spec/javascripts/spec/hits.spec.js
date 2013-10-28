f79c28e1196d1bdc3cc1de4c59aeb3ad25277b0a
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2013-10-28 10:43:04 +0000

    Reduce fragileness of hits JS spec
    * Remove unnecessary load tests
    * Remove unnecessary chart.draw tests
    * Be clearer about testing the data used and the container to draw into

--- spec/javascripts/spec/hits.spec.js
+++ spec/javascripts/spec/hits.spec.js
@@ -19,7 +19,6 @@ describe('A hits module', function() {
           LineChart: function() {}
         }
       }
-
     });
 
     afterEach(function() {
@@ -28,50 +27,40 @@ describe('A hits module', function() {
     });
 
     it('waits for google charts API to load' , function() {
-
-      spyOn(window.google, "load");
       spyOn(window.google, "setOnLoadCallback");
 
       root.GOVUK.Hits.plot();
-
-      expect(window.google.load).toHaveBeenCalledWith("visualization", "1", {packages:["corechart"]});
       expect(window.google.setOnLoadCallback).toHaveBeenCalledWith(jasmine.any(Function));
     });
 
     describe('when the chart API has loaded', function() {
 
+      var callbackFn;
+
       beforeEach(function() {
         window.rawData = [["Date", "Errors"], ["2012-10-17", 200], ["2012-10-18", 810]];
-      });
-
-      it('parses the raw data and draws it', function() {
-
-        var callbackFn,
-            chart = {draw: function() {}};
 
         spyOn(window.google, "setOnLoadCallback").and.callFake(function(callback) {
           callbackFn = callback;
         });
 
-        spyOn(window.google.visualization, "arrayToDataTable");
-        spyOn(window.google.visualization, "LineChart").and.returnValue(chart);
-        spyOn(chart, "draw");
-
+        spyOn(window.google.visualization, "LineChart").and.returnValue({draw: function() {}});
         root.GOVUK.Hits.plot();
-        callbackFn();
+      });
 
+      it('parses the raw data provided by the page', function() {
+        spyOn(window.google.visualization, "arrayToDataTable");
+        callbackFn();
+        
         expect(window.google.visualization.arrayToDataTable).toHaveBeenCalledWith(window.rawData);
-        expect(window.google.visualization.LineChart).toHaveBeenCalled();
+      });
+
+      it('draws into the hits graph container', function() {
+        callbackFn();
 
         var call = window.google.visualization.LineChart.calls.mostRecent();
         expect($(call.args[0]).is('.js-hits-graph')).toBe(true);
-
-        expect(chart.draw).toHaveBeenCalled();
-
       });
-
     });
-
   });
-
 });
