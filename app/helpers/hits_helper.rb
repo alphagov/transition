76afb57895aa7ebdf01c180ce0741feceffc7567
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-10-29 11:46:59 +0000

    Minor hits_helper readability tidy

--- app/helpers/hits_helper.rb
+++ app/helpers/hits_helper.rb
@@ -1,5 +1,3 @@
-require 'json'
-
 module HitsHelper
   def link_to_hit(hit)
     scheme_and_host = 'http://'+ hit.host.hostname
@@ -21,33 +19,37 @@ module HitsHelper
   #          {label: 'Errors', type: 'number'},
   #          {label: 'Archive', type: 'number'}],
   #   rows: [
-  #          {c:[{v: 'Date(2012, 12, 10)'}, {v: 10000, f:'10,000'}, {v: 20000, f:'20,000'}]},
-  #          {c:[{v: 'Date(2012, 12, 11)'}, {v: 1000,  f:'1,000'}, {v: 2000, f:'2,000'}]},
+  #          {c:[{v: 'Date(2012, 11, 10)'}, {v: 10000, f:'10,000'}, {v: 20000, f:'20,000'}]},
+  #          {c:[{v: 'Date(2012, 11, 11)'}, {v: 1000,  f:'1,000'}, {v: 2000, f:'2,000'}]},
   #         ]
   # }
   def google_data_table(categories)
-
     dates = {}
-    cols = [{label: 'Date', type: 'date'}]
+    cols  = [{ label: 'Date', type: 'date' }]
 
     categories.each do |category|
-      cols << {label: category.title, type: 'number'}
+      cols << { label: category.title, type: 'number' }
 
       category.points.each do |hit|
-        date = dates[hit.hit_on] || dates[hit.hit_on] = {}
+        date                = dates[hit.hit_on] || (dates[hit.hit_on] = {})
         date[category.name] = hit.count
       end
     end
 
     rows = []
     dates.each_pair do |date, category_counts|
-      rows << {c: [
-                {v: "Date(#{date.year}, #{date.month - 1}, #{date.day})"},
-                *categories.map {|c| {v: category_counts[c.name] || 0, f: number_with_delimiter(category_counts[c.name] || 0)}}
-              ]}
+      rows << {
+        c: [
+             { v: "Date(#{date.year}, #{date.month - 1}, #{date.day})" },
+             *categories.map do |c|
+               count_for_category = category_counts[c.name] || 0
+               { v: count_for_category, f: number_with_delimiter(count_for_category) }
+             end
+           ]
+      }
     end
 
-    {cols: cols, rows: rows}.to_json.html_safe
+    { cols: cols, rows: rows }.to_json.html_safe
   end
 
   def colors(point_categories)
