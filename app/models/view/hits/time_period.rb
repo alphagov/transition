f9d944d0f261d369dc0ffbf6aa055f1fb3930e01
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-10-31 18:37:03 +0000

    Add a View::Hits::TimePeriod object
    Helps with rendering the view, and also allows us to look up
    a pre-cooked time period by slug.

---
+++ app/models/view/hits/time_period.rb
@@ -0,0 +1,54 @@
+module View
+  module Hits
+    ##
+    # The fixed time periods available to view
+    class TimePeriod < Struct.new(:title, :range_proc)
+      def self.slugize(title)
+        title.downcase.gsub(' ', '-')
+      end
+
+      PERIODS_BY_SLUG = {
+        'Yesterday'       => lambda { Date.yesterday..Date.today },
+        'Last seven days' => lambda { 7.days.ago.to_date..Date.today },
+        'Last 30 days'    => lambda { 30.days.ago.to_date..Date.today },
+        'All time'        => lambda { 100.years.ago.to_date..Date.today }
+      }.inject({}) do |hash, arr|
+        title, range_proc = *arr
+        hash[TimePeriod.slugize(title)] = TimePeriod.new(title, range_proc)
+        hash
+      end
+
+      def self.all
+        PERIODS_BY_SLUG.values
+      end
+
+      def self.default
+        PERIODS_BY_SLUG['all-time']
+      end
+
+      def self.[](slug)
+        PERIODS_BY_SLUG[slug]
+      end
+
+      def slug
+        TimePeriod.slugize(title)
+      end
+
+      def query_slug
+        slug unless slug == 'all-time'
+      end
+
+      def range
+        range_proc.call
+      end
+
+      def start_date
+        range.min
+      end
+
+      def end_date
+        range.max
+      end
+    end
+  end
+end
---
+++ spec/models/view/time_period_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe View::Hits::TimePeriod do
+  describe '.all' do
+    subject(:all_periods) { View::Hits::TimePeriod.all }
+
+    it { should be_an(Array) }
+    it { should have(4).periods }
+
+    describe 'the first' do
+      subject { View::Hits::TimePeriod.all.first }
+
+      it { should be_a(View::Hits::TimePeriod) }
+
+      its(:title)      { should == 'Yesterday' }
+      its(:slug)       { should == 'yesterday' }
+      its(:query_slug) { should == 'yesterday' }
+    end
+
+    describe 'the default' do
+      subject { View::Hits::TimePeriod.default }
+
+      its(:title)      { should eql('All time') }
+      its(:slug)       { should eql('all-time') }
+      its(:query_slug) { should be_nil }
+    end
+
+    describe 'indexing on slug' do
+      it 'returns nil on unrecognised time periods' do
+        View::Hits::TimePeriod['non-existent'].should be_nil
+      end
+
+      describe 'Last 30 days' do
+        subject { View::Hits::TimePeriod['last-30-days'] }
+
+        its(:title)      { should == 'Last 30 days' }
+        its(:range)      { should == (30.days.ago.to_date..Date.today) }
+        its(:start_date) { should == 30.days.ago.to_date }
+        its(:end_date)   { should == Date.today }
+
+        it 'calculates dates correctly even if, say, the server has been up a few decades' do
+          Timecop.freeze(Date.new(2112, 10, 31)) do
+            View::Hits::TimePeriod['last-30-days'].range.should == (30.days.ago.to_date..Date.today)
+          end
+        end
+      end
+    end
+  end
+end
