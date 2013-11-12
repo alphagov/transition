fd495815b1937fd841f093b1f286a50e8fc09678
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2013-11-12 17:51:36 +0000

    Add an aka_hostname method to Host
    We don't want or need to store the aka hostnames as separate hosts
    for a site, because we don't need to use them within the app. They
    are needed for configuring the CDN, but we can generate them for
    that purpose from the real hostnames.

--- app/models/host.rb
+++ app/models/host.rb
@@ -2,4 +2,11 @@ class Host < ActiveRecord::Base
   belongs_to :site
   has_many :hits
   has_many :daily_hit_totals
+
+  def aka_hostname
+    # This does the reverse of Bouncer's aka handling:
+    #     hostname.sub(/^aka-/, '').sub(/^aka\./, 'www.')
+    aka = hostname.sub(/^www\./, 'aka.')
+    aka.start_with?('aka.') ? aka : 'aka-' + aka
+  end
 end
--- spec/models/host_spec.rb
+++ spec/models/host_spec.rb
@@ -4,4 +4,19 @@ describe Host do
   describe 'relationships' do
     it { should belong_to(:site) }
   end
+
+  describe '#aka_hostname' do
+    let(:host) { build(:host, hostname: @hostname) }
+    subject(:aka_hostname) { host.aka_hostname }
+
+    it "should add aka- on the front if no www" do
+      @hostname = 'foo.com'
+      aka_hostname.should eql('aka-foo.com')
+    end
+
+    it "should replace www. with aka. " do
+      @hostname = 'www.foo.com'
+      aka_hostname.should eql('aka.foo.com')
+    end
+  end
 end
