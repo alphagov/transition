13aba50d47eccc183fac742132d31c4f19861b47
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2013-11-13 17:47:07 +0000

    Test hosts presenter in hosts presenter tests
    The main logic for generating the response was moved from the
    hosts controller into the hosts presenter, so the tests for it
    should also be moved.

--- spec/controllers/hosts_controller_spec.rb
+++ spec/controllers/hosts_controller_spec.rb
@@ -22,26 +22,5 @@ describe HostsController do
         expect(@parsed_response).to have_key(key)
       end
     end
-
-    it 'provides the hostname and managed_by_transition for a host' do
-      first_host = @parsed_response['results'].first
-      expect(first_host).to have_key('hostname')
-      expect(first_host).to have_key('managed_by_transition')
-    end
-
-    it 'contains twice the number of hosts (to include aka hostnames)' do
-      expect(@parsed_response['results'].count).to be(6)
-    end
-
-    it 'contains the actual and aka hostnames for each stored host' do
-      results = @parsed_response['results']
-      hostnames = results[0..1].map { |h| h['hostname'] }
-      expected_hostnames =
-        [
-          site.default_host.hostname,
-          site.default_host.aka_hostname,
-        ]
-      expect(hostnames).to eql(expected_hostnames)
-    end
   end
 end
