6278dd0df09dbe5b5e63a52ae9bfde4fc4f6385c
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-11-13 17:48:29 +0000

    Rudimentary Whitehall document URLs importer

---
+++ spec/lib/transition/import/whitehall_document_urls_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+require 'transition/import/whitehall_document_urls'
+
+
+def csv_for(old_path, govuk_path, whitehall_state = 'published')
+  StringIO.new(<<-END)
+Old Url,New Url,Status,Slug,Admin Url,State
+http://www.dft.gov.uk#{old_path},https://www.gov.uk#{govuk_path},301,new,http://whitehall-admin/#{rand(1000)},#{whitehall_state}
+      END
+end
+
+describe Transition::Import::WhitehallDocumentURLs do
+  describe 'from_csv' do
+    context 'published documents' do
+      it 'adds a new mapping' do
+        site = create(:site_with_default_host, abbr: 'www.dft')
+        csv =  csv_for('/oldurl','/new')
+
+        Transition::Import::WhitehallDocumentURLs.new.from_csv(csv)
+        Mapping.all.count.should == 1
+        mapping = Mapping.first
+        mapping.site.should == site
+        mapping.path.should == '/oldurl'
+        mapping.new_url.should == 'https://www.gov.uk/new'
+        mapping.http_status.should == '301'
+      end
+
+      it 'canonicalises the URL before creating a new mapping' do
+        site = create(:site_with_default_host, abbr: 'www.dft', query_params: 'significant')
+        csv =  csv_for('/oldurl?significant=aha&ignored=ohyes','/new')
+
+        Transition::Import::WhitehallDocumentURLs.new.from_csv(csv)
+        Mapping.all.count.should == 1
+        mapping = Mapping.first
+        mapping.site.should == site
+        mapping.path.should == '/oldurl?significant=aha'
+        mapping.new_url.should == 'https://www.gov.uk/new'
+        mapping.http_status.should == '301'
+      end
+
+      it 'canonicalises the URL to find the one to update'
+
+      it 'skips rows without an Old Url'
+
+      it 'logs an unknown host'
+
+      it 'handles parse failures of Old Url'
+    end
+  end
+end
