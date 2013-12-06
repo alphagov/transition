75768ec66aa2616d56d89e568c99c44518de846c
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-12-06 13:11:42 +0000

    Fix schedule.rb to run the right thing
    It used to be a script, but I neglected to change this file to match when it
    became a rake task.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # default cron env is "/usr/bin:/bin" which is not sufficient as govuk_env is in /usr/local/bin
 env :PATH, '/usr/local/bin:/usr/bin:/bin'
 set :output, {:error => 'log/cron.error.log', :standard => 'log/cron.log'}
-job_type :run_script, 'cd :path && RAILS_ENV=:environment /usr/local/bin/govuk_setenv transition script/:task :output'
+job_type :rake, 'cd :path && /usr/local/bin/govuk_setenv transition bundle exec rake :task :output'
 
 # Run when the file has just been generated.
 # It appears to take ~50 minutes to generate based on the Last Modified HTTP header
@@ -9,5 +9,5 @@ job_type :run_script, 'cd :path && RAILS_ENV=:environment /usr/local/bin/govuk_s
 # At the time of writing, the file is generated twice a day:
 # https://github.com/alphagov/whitehall/blob/master/config/schedule.rb#L18
 every :day, at: ['3:30am', '1:15pm'] do
-  run_script 'import_whitehall_mappings'
+  rake 'import:whitehall:mappings'
 end
