4be10fb0ee298070359f9f92c7f129867f1fcbd3
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-12-06 15:52:38 +0000

    Org controller spec needs sites to pass now

--- spec/controllers/organisation_controller_spec.rb
+++ spec/controllers/organisation_controller_spec.rb
@@ -3,13 +3,10 @@ require 'spec_helper'
 describe OrganisationsController do
   describe '#index' do
 
-    let(:organisation_z) { create :organisation, title: 'Zzzzzz' }
-    let(:organisation_a) { create :organisation, title: 'Aaaaaa' }
+    let!(:organisation_z) { create :organisation, :with_site, title: 'Zzzzzz' }
+    let!(:organisation_a) { create :organisation, :with_site, title: 'Aaaaaa' }
 
     before do
-      organisation_z.should be_persisted
-      organisation_a.should be_persisted
-
       login_as_stub_user
       get :index
     end
