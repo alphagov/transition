6f5752a2394fec0487c29b6e93eaf96ec2382b92
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2013-12-24 13:28:04 +0000

    Cleanup version and changeset display
    * Generate a human readable title based on the type of changes
    * Use ordered lists, definition lists and bootstrap wells rather than
    nested tables
    * Use friendlier dates
    
    Caveats are that the logic for displaying the changes is a little ack n
    slash and could do with some love. Most of this comes as result of our
    models no longer matching displayed labels.

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,5 +1,24 @@
+#encoding: utf-8
+
 module VersionsHelper
   def value_or_blank(value)
     value.blank? ? content_tag(:span, class: 'blank') { '<blank>' } : value
   end
+
+  def changeset_title(version)
+    if version.changeset['http_status']
+      if version.changeset['http_status'][0] == '410'
+        "Archive → Redirect"
+      else
+        "Redirect → Archive"
+      end
+    elsif version.changeset.length == 1
+      first = version.changeset.first[0].titleize
+      first = "Alternative Archive URL" if first == "Archive URL"
+      "#{first} updated"
+    else
+      "Multiple properties updated"
+    end
+  end
+
 end
