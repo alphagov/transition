5516772f8b3ad2027679e168112338a2d6e5ebe1
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-01-07 16:08:56 +0000

    Canonicalize paths extracted from input

--- app/models/view/mappings/bulk_adder.rb
+++ app/models/view/mappings/bulk_adder.rb
@@ -6,10 +6,14 @@ module View
     #
     # Needs a site and params to work out what should be created
     class BulkAdder < Struct.new(:site, :params, :back_to_index)
-      def paths
+      def raw_paths
         # Efficiently match any combination of new line characters:
         #     http://stackoverflow.com/questions/10805125
         params[:paths].split(/\r?\n|\r/).select { |p| p.present? }
+      end
+
+      def canonical_paths
+        raw_paths.map { |p| site.canonical_path(p) }.select { |p| p.present? }.uniq
       end
     end
   end
--- spec/models/view/bulk_adder_spec.rb
+++ spec/models/view/bulk_adder_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe View::Mappings::BulkAdder do
-  describe '#paths' do
+  describe '#raw_paths' do
     let!(:site) { create(:site) }
-    subject(:paths) { View::Mappings::BulkAdder.new(site, { paths: @paths_input }, '').paths }
+    subject { View::Mappings::BulkAdder.new(site, { paths: @paths_input }, '').raw_paths }
 
     context 'empty string' do
       before { @paths_input = '' }
@@ -44,6 +44,40 @@ describe View::Mappings::BulkAdder do
           "/sitecontent/documents/countries/491163/pvs-dis?view=binary",
           "/about us with spaces",
           "/arbitrary%20punctuation%3E"
+        ]
+      )}
+    end
+  end
+
+  describe '#canonical_paths' do
+    let!(:site) { create(:site) }
+    subject { View::Mappings::BulkAdder.new(site, { paths: @paths_input }, '').canonical_paths }
+
+    describe 'multiple raw paths are canonicalized' do
+      before { @paths_input = "/sitecontent/documents/countries/491163/pvs-dis?view=binary\n/about us with spaces\n/LOWER" }
+
+      it { should eql(
+        [
+          "/sitecontent/documents/countries/491163/pvs-dis",
+          "/about%20us%20with%20spaces",
+          "/lower",
+        ]
+      )}
+    end
+
+    describe 'paths which canonicalize to empty strings are ignored' do
+      before { @paths_input = "noslash" }
+
+      it { should eql([]) }
+    end
+
+    describe 'paths which are duplicated once canonicalized are de-duplicated' do
+      before { @paths_input = "/about\n/another\n/about?view=on\n/about?view=off" }
+
+      it { should eql(
+        [
+          "/about",
+          "/another",
         ]
       )}
     end
