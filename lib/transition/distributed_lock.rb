cf5249967e625185b96ead3c46191fbf6c3f4b45
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-01-16 17:00:34 +0000

    Use a distributed lock for updating DNS details
    Let's not do work on every machine that only needs to be done on one.

---
+++ lib/transition/distributed_lock.rb
@@ -0,0 +1,28 @@
+require 'redis'
+require 'redis-lock'
+module Transition
+  class DistributedLock
+    LIFETIME = (5 * 60) # seconds
+
+    def initialize(lock_name)
+      @lock_name = lock_name
+    end
+
+    def lock
+      redis.lock("transition:#{Rails.env}:#{@lock_name}", life: LIFETIME) do
+        Rails.logger.debug('Successfully got a lock. Running...')
+        yield
+      end
+    rescue Redis::Lock::LockNotAcquired => e
+      Rails.logger.debug("Failed to get lock for #{@lock_name} (#{e.message}). Another process probably got there first.")
+    end
+
+  private
+    def redis
+      @_redis ||= begin
+        redis_config = YAML.load_file(File.join(Rails.root, "config", "redis.yml"))
+        Redis.new(redis_config.symbolize_keys)
+      end
+    end
+  end
+end
