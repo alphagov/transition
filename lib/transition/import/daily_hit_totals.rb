ba54a1bafa61c3b70afecc3be9a694e7eb0ff321
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-02-17 14:34:06 +0000

    Make the update of daily hit totals idempotent
    * properly this time
    * Ill-advisedly, we've got a column called "count". This causes some problems.
      If you refer to it, use `count` (with backticks). Not doing so means that mySQL
      will read it as the function COUNT (with no parameters). This is probably not
      what you want when operating on this table.

--- lib/transition/import/daily_hit_totals.rb
+++ lib/transition/import/daily_hit_totals.rb
@@ -2,12 +2,13 @@ module Transition
   module Import
     class DailyHitTotals
       PRECOMPUTE_TOTALS_FROM_HITS = <<-mySQL
-        INSERT IGNORE INTO daily_hit_totals (host_id, http_status, count, total_on)
+        INSERT INTO daily_hit_totals (host_id, http_status, `count`, total_on)
         (
-          SELECT host_id, http_status, SUM(count) as count, hit_on
+          SELECT host_id, http_status, SUM(count) as `count`, hit_on
           FROM hits
           GROUP BY host_id, http_status, hit_on
         )
+        ON DUPLICATE KEY UPDATE `count` = VALUES(`count`)
       mySQL
 
       def self.from_hits!
