6a2051c4488c6fee1414ee188b899e103614a3d5
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2014-02-19 10:43:05 +0000

    Refine glossary copy and provide examples
    * Include clear examples of archives, redirects and sites
    * Show HTTP statuses in a table
    * Increase cross-linking between entries
    * Clarify Path and Query parameter terms

--- app/controllers/glossary_controller.rb
+++ app/controllers/glossary_controller.rb
@@ -1,6 +1,11 @@
 class GlossaryController < ApplicationController
 
   def index
+
+    @example_site = Site.where(abbr: 'cabinetoffice').first
+    @example_archive = Mapping.where(http_status: '410').first
+    @example_redirect = @example_site.mappings.where(http_status: '301').first
+
   end
 
 end
