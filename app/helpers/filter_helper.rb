10031a876525a461296fc2f787f8be60c644b91b
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2014-03-01 12:18:04 +0000

    Remove the unnecessary dropdown target
    * Avoid titleize in filter helper, just use the passed in value

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -2,16 +2,15 @@ module FilterHelper
   def filter_option_link(site, type)
     link_to filter_site_mappings_path(@site),
         'class'         => 'filter-option',
-        'data-toggle' => 'dropdown',
-        'role'          => 'button',
-        'data-target' => "filter-by-#{type}" do
-      "#{type.titleize} <span class=\"glyphicon glyphicon-chevron-down\"></span>".html_safe
+        'data-toggle'   => 'dropdown',
+        'role'          => 'button' do
+      "#{type} <span class=\"glyphicon glyphicon-chevron-down\"></span>".html_safe
     end
   end
 
   def filter_remove_option_link(site, type, type_sym)
     link_to site_mappings_path(@site, params.except(type_sym, :page)), class: 'filter-option filter-selected' do
-      "<span class=\"glyphicon glyphicon-remove\"></span> #{type.titleize}".html_safe
+      "<span class=\"glyphicon glyphicon-remove\"></span> #{type}".html_safe
     end
   end
 
