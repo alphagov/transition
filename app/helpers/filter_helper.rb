c1032d54642bae52c49db7a6f30d3b8d154af2e3
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2014-03-01 14:18:33 +0000

    Include link to remove all tag filters

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,7 +1,9 @@
 module FilterHelper
-  def filter_option_link(site, type)
+  def filter_option_link(site, type, options = {})
+    selected = options[:selected]
+
     link_to filter_site_mappings_path(@site),
-        'class'         => 'filter-option',
+        'class'         => "filter-option #{'filter-selected' if selected}",
         'data-toggle'   => 'dropdown',
         'role'          => 'button' do
       "#{type} <span class=\"glyphicon glyphicon-chevron-down\"></span>".html_safe
@@ -9,8 +11,8 @@ module FilterHelper
   end
 
   def filter_remove_option_link(site, type, type_sym)
-    link_to site_mappings_path(@site, params.except(type_sym, :page)), class: 'filter-option filter-selected' do
-      "<span class=\"glyphicon glyphicon-remove\"></span> #{type}".html_safe
+    link_to site_mappings_path(@site, params.except(type_sym, :page)), title: 'Remove filter', class: 'filter-option filter-selected' do
+      "<span class=\"glyphicon glyphicon-remove\"></span><span class=\"rm\">Remove</span> #{type}".html_safe
     end
   end
 
