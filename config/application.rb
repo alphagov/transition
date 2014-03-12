14048562489f53b54c8f5a0bb26464e21595593e
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2014-03-12 18:11:06 +0000

    Add respond.js for correct <IE9 rendering

--- config/application.rb
+++ config/application.rb
@@ -72,5 +72,6 @@ module Transition
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.assets.precompile += %w(html5.js)
+    config.assets.precompile += %w(respond.min.js)
   end
 end
