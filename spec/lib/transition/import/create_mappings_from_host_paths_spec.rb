3e3736882f834da14fb5d7b6a5d54cf41e13841c
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-03-13 15:47:57 +0000

    More tests for creating mappings from HostPaths

--- spec/lib/transition/import/create_mappings_from_host_paths_spec.rb
+++ spec/lib/transition/import/create_mappings_from_host_paths_spec.rb
@@ -7,6 +7,13 @@ describe Transition::Import::CreateMappingsFromHostPaths do
     @host = @site.hosts.first # default site factory creates a host
   end
 
+  after do
+    # HostPath is MyISAM. MyISAM doesn't support transactions so
+    # DatabaseCleaner's strategy of using transactions and rolling back doesn't
+    # work.
+    HostPath.delete_all
+  end
+
   context 'a site with no HostPaths' do
     it 'should do nothing' do
       Transition::Import::CreateMappingsFromHostPaths.call(@site)
@@ -42,6 +49,50 @@ describe Transition::Import::CreateMappingsFromHostPaths do
     # hits_mappings_relations should be run to do this.
     it 'should not modify the host_path' do
       @host_path.mapping_id.should be_nil
+    end
+  end
+
+  context 'a HostPath with a matching mapping' do
+    before do
+      path = "/foo?insignificant=1"
+      c14n_path = @site.canonical_path(path)
+      @host_path = create(:host_path,
+          path: path,
+          host: @host,
+          path_hash: Digest::SHA1.hexdigest(path),
+          c14n_path_hash: Digest::SHA1.hexdigest(c14n_path))
+      @mapping = create(:redirect, path: c14n_path, site: @site)
+      Transition::Import::CreateMappingsFromHostPaths.call(@site)
+    end
+
+    it 'should not create any more mappings' do
+      Mapping.count.should eql(1)
+    end
+
+    describe 'the (unchanged) existing mapping' do
+      subject { @mapping }
+
+      its(:http_status) { should eql('301') }
+    end
+  end
+
+  context 'a site with multiple hosts' do
+    before do
+      @site.hosts << @second_host = build(:host)
+      @site.hosts.each do |host|
+        path = "/foo-on-#{host.hostname}"
+        c14n_path = @site.canonical_path(path)
+        host_path = create(:host_path,
+            path: path,
+            host: host,
+            path_hash: Digest::SHA1.hexdigest(path),
+            c14n_path_hash: Digest::SHA1.hexdigest(c14n_path))
+      end
+      Transition::Import::CreateMappingsFromHostPaths.call(@site)
+    end
+
+    it 'should create mappings for HostPaths for each host' do
+      @site.mappings.count.should eql(2)
     end
   end
 end
