bc8bac26ede276c62f0f1e4c45e75cd44c7f25c3
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-03-14 17:04:57 +0000

    Refactor to reduce unnecessary queries

--- lib/transition/import/create_mappings_from_host_paths.rb
+++ lib/transition/import/create_mappings_from_host_paths.rb
@@ -2,10 +2,11 @@ module Transition
   module Import
     class CreateMappingsFromHostPaths
       def self.call(site)
-        site.hosts.each do |host|
-          host.host_paths.where('mapping_id is null').each do |host_path|
-            site.mappings.create(path: host_path.path, http_status: '410')
-          end
+        site_paths = site.host_paths.where('mapping_id is null').group('c14n_path_hash').pluck(:path)
+        site_paths.map do |uncanonicalised_path|
+          # Try to create them (there may be duplicates in the set and they may
+          # already exist).
+          site.mappings.create(path: uncanonicalised_path, http_status: '410')
         end
       end
     end
