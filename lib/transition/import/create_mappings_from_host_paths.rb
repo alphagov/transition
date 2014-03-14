ee5874ad02812fb49483712dcbb49fe6e6051e1d
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-03-14 17:19:10 +0000

    Provide feedback when creating mappings

--- lib/transition/import/create_mappings_from_host_paths.rb
+++ lib/transition/import/create_mappings_from_host_paths.rb
@@ -1,12 +1,20 @@
+require 'transition/import/console_job_wrapper'
+
 module Transition
   module Import
     class CreateMappingsFromHostPaths
+      extend Transition::Import::ConsoleJobWrapper
+
       def self.call(site)
-        site_paths = site.host_paths.where('mapping_id is null').group('c14n_path_hash').pluck(:path)
-        site_paths.map do |uncanonicalised_path|
-          # Try to create them (there may be duplicates in the set and they may
-          # already exist).
-          site.mappings.create(path: uncanonicalised_path, http_status: '410')
+        start 'Creating mappings from HostPaths' do
+          site_paths = site.host_paths.where('mapping_id is null').group('c14n_path_hash').pluck(:path)
+          site_paths.map do |uncanonicalised_path|
+            # Try to create them (there may be duplicates in the set and they may
+            # already exist).
+            if site.mappings.create(path: uncanonicalised_path, http_status: '410')
+              $stderr.print '.'
+            end
+          end
         end
       end
     end
