9e45240d728b5a6765899fa766abd427bdb2a6c3
Paul Hayes <paul.hayes@digital.cabinet-office.gov.uk>
2014-03-14 14:24:22 +0000

    Use the passed in site value in filter helpers
    * Subtly different, might have caused problems down the line

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -2,7 +2,7 @@ module FilterHelper
   def filter_option_link(site, type, options = {})
     selected = options[:selected]
 
-    link_to filter_site_mappings_path(@site),
+    link_to filter_site_mappings_path(site),
         'class'         => "filter-option #{'filter-selected' if selected}",
         'data-toggle'   => 'dropdown',
         'role'          => 'button' do
@@ -11,7 +11,7 @@ module FilterHelper
   end
 
   def filter_remove_option_link(site, type, type_sym)
-    link_to site_mappings_path(@site, params.except(type_sym, :page)), title: 'Remove filter', class: 'filter-option filter-selected' do
+    link_to site_mappings_path(site, params.except(type_sym, :page)), title: 'Remove filter', class: 'filter-option filter-selected' do
       "<span class=\"glyphicon glyphicon-remove\"></span><span class=\"rm\">Remove</span> #{type}".html_safe
     end
   end
