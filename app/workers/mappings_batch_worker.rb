25b57f34906435e22b4266049297a955cb3dcaa2
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-04-22 16:02:18 +0000

    Handle batches being deleted before processing

--- app/workers/mappings_batch_worker.rb
+++ app/workers/mappings_batch_worker.rb
@@ -2,7 +2,8 @@ class MappingsBatchWorker
   include Sidekiq::Worker
 
   def perform(mappings_batch_id)
-    batch = MappingsBatch.find(mappings_batch_id)
+    batch = MappingsBatch.find_by_id(mappings_batch_id)
+    return if batch.nil?
     Transition::History.as_a_user(batch.user) do
       batch.process
     end
--- spec/workers/mappings_batch_worker_spec.rb
+++ spec/workers/mappings_batch_worker_spec.rb
@@ -13,5 +13,11 @@ describe MappingsBatchWorker do
       its(:whodunnit) { should eql('Bob') }
       its(:user_id)   { should eql(user.id) }
     end
+
+    context 'batch being deleted before processing' do
+      it 'should not raise an error' do
+        expect{MappingsBatchWorker.new.perform(1234)}.to_not raise_error
+      end
+    end
   end
 end
