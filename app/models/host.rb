47a396c41b1db42b5941718f29e5d25bcf88cff0
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2014-04-28 16:06:57 +0100

    Validate that a non-aka host has no canonical_host_id

--- app/models/host.rb
+++ app/models/host.rb
@@ -8,7 +8,7 @@ class Host < ActiveRecord::Base
 
   validates :hostname, presence: true
   validates :site, presence: true
-  validates :canonical_host_id, presence: { :if => :aka? }
+  validate :canonical_host_id_xor_aka_present
 
   scope :excluding_aka, where('hostname not like "aka%"')
 
@@ -36,5 +36,15 @@ class Host < ActiveRecord::Base
 
   def redirected_by_gds?
     /^redirector-cdn[^.]*\.production\.govuk\.service\.gov\.uk$/.match(cname).present?
+  end
+
+  def canonical_host_id_xor_aka_present
+    # exclusive or: one and only one of canonical_host_id and aka? is required
+    if !aka? && canonical_host_id.present?
+      errors[:canonical_host_id] << 'must be blank for a non-aka host'
+    end
+    if aka? && canonical_host_id.blank?
+      errors[:canonical_host_id] << 'can\'t be blank for an aka host'
+    end
   end
 end
