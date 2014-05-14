41b81ab4adf86485094e1a76be0829484e6b0010
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-05-14 13:34:15 +0000

    Admin UI for viewing/adding to Whitelisted Hosts

---
+++ app/controllers/admin/whitelisted_hosts_controller.rb
@@ -0,0 +1,20 @@
+class Admin::WhitelistedHostsController < Admin::AdminController
+  include PaperTrail::Rails::Controller
+
+  def index
+    @whitelisted_hosts = WhitelistedHost.order(:hostname)
+  end
+
+  def new
+    @whitelisted_host = WhitelistedHost.new
+  end
+
+  def create
+    @whitelisted_host = WhitelistedHost.new(params[:whitelisted_host])
+    if @whitelisted_host.save
+      redirect_to admin_whitelisted_hosts_path, flash: { success: "#{@whitelisted_host.hostname} added to whitelist." }
+    else
+      render :new
+    end
+  end
+end
--- app/models/whitelisted_host.rb
+++ app/models/whitelisted_host.rb
@@ -1,6 +1,15 @@
 class WhitelistedHost < ActiveRecord::Base
   attr_accessible :hostname
 
+  has_paper_trail
+
+  before_save :ensure_papertrail_user_config
+
   validates :hostname, presence: true
   validates :hostname, hostname: true
+  validates :hostname, uniqueness: true
+
+  def ensure_papertrail_user_config
+    Transition::History.ensure_user!
+  end
 end
