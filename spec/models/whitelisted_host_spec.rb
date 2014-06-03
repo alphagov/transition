4257b961a685e000a0436b77fbfc307dffe61185
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-06-03 12:09:08 +0000

    Disallow whitelist entries that would be redundant

--- spec/models/whitelisted_host_spec.rb
+++ spec/models/whitelisted_host_spec.rb
@@ -6,11 +6,22 @@ describe WhitelistedHost do
     it { should validate_uniqueness_of(:hostname).with_message('is already in the list') }
 
     describe 'hostname' do
-      subject(:whitelisted_host) { build(:whitelisted_host, hostname: 'a.gov.uk/') }
+      context 'is invalid' do
+        subject(:whitelisted_host) { build(:whitelisted_host, hostname: 'a.gov.uk/') }
 
-      its(:valid?) { should be_false }
-      it 'should have an error for invalid hostname' do
-        whitelisted_host.errors_on(:hostname).should include('is an invalid hostname')
+        its(:valid?) { should be_false }
+        it 'should have an error for invalid hostname' do
+          whitelisted_host.errors_on(:hostname).should include('is an invalid hostname')
+        end
+      end
+
+      context 'is automatically allowed anyway' do
+        subject(:whitelisted_host) { build(:whitelisted_host, hostname: 'a.gov.uk') }
+
+        its(:valid?) { should be_false }
+        it 'should have an error' do
+          whitelisted_host.errors_on(:hostname).should include('cannot end in .gov.uk or .mod.uk - these are automatically whitelisted')
+        end
       end
     end
   end
