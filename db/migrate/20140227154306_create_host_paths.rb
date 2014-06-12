59892893b9b88bb68871d48a8cbee4ff63b43f07
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-06-12 15:35:20 +0100

    Convert config/migrations to PostgreSQL
    * Remove structure.sql, add schema.rb, configure appropriately
    * Remove mySQL-specific option values from migrations
    * Remove fix_collation_woes, as is all mySQL-specific
    * Remove mysql2 gem in favour of pg

--- db/migrate/20140227154306_create_host_paths.rb
+++ db/migrate/20140227154306_create_host_paths.rb
@@ -1,6 +1,6 @@
 class CreateHostPaths < ActiveRecord::Migration
   def change
-    create_table :host_paths, options: 'DEFAULT CHARSET=utf8 COLLATE=utf8_bin ENGINE=MyISAM' do |t|
+    create_table :host_paths do |t|
       t.string :path, limit: 2048
       t.string :path_hash
       t.string :c14n_path_hash
@@ -9,7 +9,7 @@ class CreateHostPaths < ActiveRecord::Migration
       t.references :mapping
     end
 
-    add_index :host_paths, [:host_id, :path_hash], unique: true # Used only for INSERT IGNORE
+    add_index :host_paths, [:host_id, :path_hash], unique: true # Used only for uniqueness inserting
     add_index :host_paths, :c14n_path_hash                      # Used for lookup when creating/editing mappings
     add_index :host_paths, :mapping_id
   end
