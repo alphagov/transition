6a17c681471f3693c15b10b704f7356c50c996ff
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-06-12 15:35:20 +0100

    Convert config/migrations to PostgreSQL
    * Remove structure.sql, add schema.rb, configure appropriately
    * Remove mySQL-specific option values from migrations
    * Remove mysql2 gem in favour of pg
    * Empty the migration which fixed MySQL collation
      This migration was used to fix some collation inconsistencies in
      MySQL but we don't need its functionality on PostgreSQL. The migration
      will remain in the schema_migrations table in the database, so we're
      leaving this file in place but with empty methods to avoid the
      potentially confusing '********** NO FILE **********' message shown by
      `rake db:migrate:status`.

--- db/migrate/20140611144610_fix_collation_woes.rb
+++ db/migrate/20140611144610_fix_collation_woes.rb
@@ -1,19 +1,8 @@
 class FixCollationWoes < ActiveRecord::Migration
   def up
-    statements = [
-      'ALTER TABLE mappings_batch_entries MODIFY path varchar(2048) COLLATE utf8_unicode_ci;',
-
-      'ALTER TABLE mappings_batches MODIFY new_url varchar(255) COLLATE utf8_unicode_ci;',
-      'ALTER TABLE mappings_batches MODIFY tag_list varchar(255) COLLATE utf8_unicode_ci;',
-      'ALTER TABLE mappings_batches MODIFY state varchar(255) COLLATE utf8_unicode_ci DEFAULT "unqueued";',
-      'ALTER TABLE mappings_batches MODIFY type varchar(255) COLLATE utf8_unicode_ci;',
-
-      'ALTER TABLE sessions MODIFY session_id varchar(255) COLLATE utf8_unicode_ci NOT NULL;',
-      'ALTER TABLE sessions MODIFY data text COLLATE utf8_unicode_ci;',
-    ]
-    statements.each do |statement|
-      ActiveRecord::Base.connection.execute(statement)
-    end
+    # This migration fixed some collation inconsistencies for MySQL, but has
+    # been emptied on switching to PostgreSQL. The file remains so that we don't
+    # have a migration record in the database without a matching file.
   end
 
   def down
