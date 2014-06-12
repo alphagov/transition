ea2e67786018810566fbbf1d326e0f5a45e46ff3
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-06-12 15:35:20 +0100

    Convert config/migrations to PostgreSQL
    * Remove structure.sql, add schema.rb, configure appropriately
    * Remove mySQL-specific option values from migrations
    * Remove mysql2 gem in favour of pg
    * Empty the migration which fixed MySQL collation
      This migration was used to fix some collation inconsistencies in
      MySQL but we don't need its functionality on PostgreSQL. The migration
      will remain in the schema_migrations table in the database, so we're
      leaving this file in place but with empty methods to avoid the
      potentially confusing '********** NO FILE **********' message shown by
      `rake db:migrate:status`.

--- db/migrate/20130926082808_create_hits_staging.rb
+++ db/migrate/20130926082808_create_hits_staging.rb
@@ -1,7 +1,7 @@
 class CreateHitsStaging < ActiveRecord::Migration
   def change
     # no indices, constraints or timestamps - this is meant to be fast
-    create_table :hits_staging, id: false, options: 'ENGINE=MyISAM' do |t|
+    create_table :hits_staging, id: false do |t|
       t.string :hostname
       t.string :path, limit: 1024
       t.string :http_status, limit: 3
