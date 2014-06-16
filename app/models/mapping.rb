d50174f4f8bf55ab5fa9a6fb5a5b8486b3b02b67
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-06-16 09:57:25 +0100

    Fix sort by hits
    * Postgres requires NULLS LAST on an ORDER DESC, apparently
    * We can get away with the GROUP BY primary key trick for the rest

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -47,16 +47,11 @@ class Mapping < ActiveRecord::Base
   validates :new_url, not_a_national_archives_url: { if: :redirect?, message: 'must not be to the National Archives. Use an archive mapping for that.' }
   validates :archive_url, national_archives_url: true
 
-  def self.with_hit_count
-    raise RuntimeError, "Postgres TODO 9: #{self}.#{__method__} - sloppy GROUP BY"
-    # See commented-out scope below
-  end
-  # scope :with_hit_count, -> {
-  #   select('mappings.*, SUM(hits.count) as hit_count').
-  #     joins('LEFT JOIN hits ON hits.mapping_id = mappings.id').
-  #     group('mappings.path_hash')
-  # }
-
+  scope :with_hit_count, -> {
+    select('mappings.*, SUM(hits.count) as hit_count').
+      joins('LEFT JOIN hits ON hits.mapping_id = mappings.id').
+      group('mappings.id')
+  }
   scope :with_type, -> type { where(type: type) }
   scope :redirects, -> { with_type('redirect') }
   scope :archives,  -> { with_type('archive') }
