f24ec817feebe82f798e38786d1df31696fc1f4c
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-06-23 16:35:48 +0100

    Refactor a tiny bit of data access in views to the controller
    - Spotted by @jamiecobbett in https://github.com/alphagov/transition/pull/329.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,6 +1,7 @@
 class SitesController < ApplicationController
   def show
     @site = Site.find_by_abbr!(params[:id])
+    @most_used_tags = @site.most_used_tags
     @hosts = @site.hosts.excluding_aka.includes(:aka_host)
   end
 end
