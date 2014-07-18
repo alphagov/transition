e0e546bb093629eb3806e87029f82b9f509b5ee8
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-07-18 16:35:14 +0000

    Move shared_examples to a dedicated directory
    Also take them out of pointless modules

---
+++ spec/support/shared_examples/authentication.rb
@@ -0,0 +1,14 @@
+shared_examples 'disallows editing by unaffiliated user' do
+  before do
+    login_as stub_user
+    make_request
+  end
+
+  it 'redirects to the index page' do
+    expect(response).to redirect_to site_mappings_path(site)
+  end
+
+  it 'sets a flash message' do
+    flash[:alert].should include('don\'t have permission to edit')
+  end
+end
