b0ee72f6364ffc1f28d8ea74530375489d96f3bd
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-07-30 15:35:21 +0100

    Fix Site#hit_total_count grouping

--- app/models/site.rb
+++ app/models/site.rb
@@ -75,7 +75,7 @@ class Site < ActiveRecord::Base
   end
 
   def hit_total_count
-    @hit_total_count ||= daily_hit_totals.select('SUM(count) AS total').first[:total].to_i
+    @hit_total_count ||= daily_hit_totals.sum(:count)
   end
 
   def update_hits_relations
