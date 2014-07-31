ac3d4b0d16f3ca93167facdc96be2f556ccccc80
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-07-31 13:32:07 +0100

    Rails 4 knows about Postgres extensions
    * So use the idiomatic migration commands (taking advantage of the fact
      there's non-dev PG DB at time of writing to re-use the original migration)
    * Remove the now-redundant explicit SQL support for specs

--- spec/support/postgres_helper.rb
+++
@@ -1,6 +0,0 @@
-RSpec.configure do |config|
-  config.before(:suite) do
-    # Extension does not get propagated to schema.rb
-    ActiveRecord::Base.connection.execute('CREATE EXTENSION if NOT EXISTS pgcrypto')
-  end
-end
