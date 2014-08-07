758646b95772001e447e98b79dbaec93d84ffb6e
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-08-07 11:47:16 +0100

    Don't let Jenkins drop the DB
    * as CREATE requires SuperUser
    * the transition test user is not a superuser

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ export RAILS_ENV=test
 export DISPLAY=":99"
 
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-bundle exec rake db:drop db:create db:structure:load
+bundle exec rake db:schema:load
 bundle exec rake assets:clean assets:precompile
 bundle exec rake
 bundle exec rake check_for_bad_time_handling
