68b144cac3193efae02c26ecd4775fd400da959e
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-09-12 11:30:05 +0100

    Use single quotes for PG strings in migration
    * PG uses double quotes as table/column signifiers

--- db/migrate/20140911113424_delete_absolute_filename_imported_hits_files.rb
+++ db/migrate/20140911113424_delete_absolute_filename_imported_hits_files.rb
@@ -1,6 +1,6 @@
 class DeleteAbsoluteFilenameImportedHitsFiles < ActiveRecord::Migration
   def up
-    command = 'DELETE FROM imported_hits_files WHERE filename LIKE "/%";'
+    command = "DELETE FROM imported_hits_files WHERE filename LIKE '/%';"
     ActiveRecord::Base.connection.execute(command)
   end
 
