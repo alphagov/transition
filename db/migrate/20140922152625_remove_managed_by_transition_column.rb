c7774d1a3707d85a22dfd3c178efd37995c04d52
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-09-22 16:32:51 +0100

    Remove the managed_by_transition column from the sites table
    - This is a part of retiring redirector.

---
+++ db/migrate/20140922152625_remove_managed_by_transition_column.rb
@@ -0,0 +1,9 @@
+class RemoveManagedByTransitionColumn < ActiveRecord::Migration
+  def up
+    remove_column :sites, :managed_by_transition
+  end
+
+  def down
+    add_column :sites, :managed_by_transition, :boolean, :null => false, :default => true
+  end
+end
