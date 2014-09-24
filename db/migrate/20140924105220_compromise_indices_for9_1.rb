fd4e2c89c87bebdb8a506d791d39bd56583353ee
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-09-24 12:25:49 +0100

    Compromise hits indices to balance 9.1 performance
    Compromise indices to make import/view hits work after discovery of
    very bad index selection on 9.1 in Preview (blocking single hit file imports
    on a single query taking > 20mins). This is an admission that
    we don't know what's gone wrong but we know how to fix it (9.1 doesn't
    select the "bad" import index when it isn't there, and hits view can use
    the two in concert).
    
    It's less than ideal, and hopefully we can go back to the two 3/4-column
    index solution for hits if we can use 9.3.x by reverting or erasing this
    commit.
    
    This is less than ideal because we know extra indices slows import down.
    On the plus side it will speed up the viewing of hits (particularly for
    summary/category) by 100-200%.

---
+++ db/migrate/20140924105220_compromise_indices_for9_1.rb
@@ -0,0 +1,33 @@
+##
+# Compromise indices to make import/view hits work after discovery of
+# very bad index selection on 9.1 in Preview. This is an admission that
+# we don't know what's gone wrong but we know how to fix it. It's
+# less than ideal, and hopefully we can go back to the two 3/4-column
+# index solution for hits if we can use 9.3.x
+#
+# This is less than ideal because we know it will slow imports down.
+# On the plus side it will speed the viewing of hits (particularly for
+# summary/category) up by 100-200%
+#
+class CompromiseIndicesFor91 < ActiveRecord::Migration
+  # A couple of helpers to avoid us having to say everything twice
+  def remove_index_if_exists(table, *args)
+    remove_index(table, *args) if index_exists?(table, *args)
+  end
+
+  def add_index_unless_exists(table, columns, options = {})
+    add_index table, columns, options unless index_exists?(table, columns, options)
+  end
+
+  def up
+    remove_index_if_exists  :hits, [:host_id, :hit_on, :http_status]
+    add_index_unless_exists :hits, [:host_id, :hit_on]
+    add_index_unless_exists :hits, [:host_id, :http_status]
+  end
+
+  def down
+    remove_index_if_exists :hits,  [:host_id, :hit_on]
+    remove_index_if_exists :hits,  [:host_id, :http_status]
+    add_index_unless_exists :hits, [:host_id, :hit_on, :http_status]
+  end
+end
