1b7b076fd74fd132cae6663401910cd1247d2b5f
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2014-10-02 10:02:40 +0000

    Add Fastly Anycast IPs
    These are the preferred IP addresses to use for all new root domains (aka
    "tlds") which we are redirecting. We will be moving domains pointed at the
    Amazon legacy IP as soon as we can.

--- app/models/host.rb
+++ app/models/host.rb
@@ -15,9 +15,10 @@ class Host < ActiveRecord::Base
 
   scope :excluding_aka, -> { where(canonical_host_id: nil) }
 
+  FASTLY_ANYCAST_IPS = ['23.235.33.144', '23.235.37.144'] # To be used for new root domains
   DYN_DNS_IPS        = ['216.146.46.10', '216.146.46.11'] # Used for a few domains we control
   AMAZON_LEGACY_IP   = ['46.137.92.159']                  # We're migrating domains off this
-  REDIRECTOR_IPS     = DYN_DNS_IPS + AMAZON_LEGACY_IP
+  REDIRECTOR_IPS     = FASTLY_ANYCAST_IPS + DYN_DNS_IPS + AMAZON_LEGACY_IP
 
   REDIRECTOR_CNAME = /^redirector-cdn[^.]*\.production\.govuk\.service\.gov\.uk$/
 
