6c158b8680d76ee1389b1f27da12ec1f2b3cbab8
Russell Garner <rgarner@zephyros-systems.co.uk>
2014-10-22 09:44:42 +0100

    Move data access to the controller
    * index.html.erb was calling ActiveRecord directly

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -3,5 +3,7 @@ class LeaderboardController < ApplicationController
     if !current_user.gds_editor?
       redirect_to root_path, flash: { notice: "Only GDS Editors can access the leaderboard." }
     end
+
+    @leaderboard = Organisation.leaderboard
   end
 end
