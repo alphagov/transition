f394bda5216c729fcd7762a0a505191c67da2d43
Isabell Long <isabell.long@digital.cabinet-office.gov.uk>
2014-10-27 16:55:28 +0000

    Order the hosts API output based on the hostname, A-Z

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -1,6 +1,6 @@
 class HostsController < ActionController::Base
   def index
-    @hosts = Host.all
+    @hosts = Host.all.order(:hostname)
 
     render json: HostsPresenter.new(@hosts).as_hash.to_json
   end
