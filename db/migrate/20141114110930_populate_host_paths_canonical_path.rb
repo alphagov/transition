eacf15503265fc05959113d2addaab6dd2aea22d
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2014-11-14 14:55:16 +0000

    Display an estimate of the time remaining.
    This will be a fairly crude estimate, and won't be particularly accurate
    if the migration speeds up or slows down as populates more rows, but
    it'll at least give us an indication of how long we've got to wait.

--- db/migrate/20141114110930_populate_host_paths_canonical_path.rb
+++ db/migrate/20141114110930_populate_host_paths_canonical_path.rb
@@ -1,21 +1,30 @@
 class PopulateHostPathsCanonicalPath < ActiveRecord::Migration
+  include ActionView::Helpers::DateHelper
+
   def up
     record_offset = 0
     batch_size    = 5000
     total_records = HostPath.where(canonical_path: nil).count
+
+    start_time = Time.zone.now
 
     host_paths = HostPath.all
       .joins(:host => :site)
       .includes(:host => :site)
       .where(canonical_path: nil)
 
+    $stderr.puts "Populating canonical paths on #{total_records} records"
     host_paths.find_in_batches(batch_size: batch_size) do |host_paths|
       host_paths.each do |host_path|
         site = host_path.host.site
         host_path.update_column(:canonical_path, site.canonical_path(host_path.path))
       end
       record_offset += batch_size
-      $stderr.puts "Updated #{record_offset} / #{total_records}"
+
+      elapsed_time = Time.zone.now - start_time
+
+      time_remaining = elapsed_time * (total_records - record_offset) / record_offset
+      $stderr.puts "Updated #{record_offset} / #{total_records} (#{distance_of_time_in_words(time_remaining)} remaining)"
     end
   end
 
