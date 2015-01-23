036526ec8c7ccb44ace4824d6d07097c53d7e4c9
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-23 16:09:18 +0000

    Exclude aka hosts from FOI export

--- script/export_for_foi/export_hosts.sql
+++ script/export_for_foi/export_hosts.sql
@@ -4,5 +4,6 @@ COPY (
     hostname
     FROM hosts
     INNER JOIN sites ON hosts.site_id = sites.id
+    WHERE canonical_host_id IS NULL
     ORDER BY sites.abbr, hostname
 ) TO STDOUT WITH DELIMITER ',' CSV HEADER;
