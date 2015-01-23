fdde70ea5cf6b9fe3ee7412c639250b1acb8035e
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-23 13:06:52 +0000

    Tweak wording

--- script/export_for_foi/export_sites.sql
+++ script/export_for_foi/export_sites.sql
@@ -7,7 +7,7 @@ COPY (
     query_params AS "Significant querystring parameters (colon separated)",
     CASE global_type WHEN 'unresolved' THEN 410 WHEN 'archive' THEN 410 WHEN 'redirect' THEN 301 END AS global_http_status,
     global_new_url AS "New URL for global 301",
-    CASE global_redirect_append_path WHEN 'f' THEN 'false' WHEN 't' THEN 'true' END AS "Append requested path to new path? (for global 301)"
+    CASE global_redirect_append_path WHEN 'f' THEN 'false' WHEN 't' THEN 'true' END AS "Append requested path to new URL? (for global 301)"
     FROM sites
     ORDER BY abbr
 ) TO STDOUT WITH DELIMITER ',' CSV HEADER;
