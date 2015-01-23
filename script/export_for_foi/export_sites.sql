c00cbf879fcea331ff19076230115fd166c9af0a
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2015-01-23 13:05:41 +0000

    Include TNA Timestamp
    Format it the way it would appear in a TNA URL.

--- script/export_for_foi/export_sites.sql
+++ script/export_for_foi/export_sites.sql
@@ -3,6 +3,7 @@ COPY (
     abbr AS "Abbreviation",
     launch_date AS "Launch Date",
     homepage AS "New Homepage",
+    to_char(tna_timestamp, 'YYYYMMDDHH24MISS') AS "TNA Timestamp",
     query_params AS "Significant querystring parameters (colon separated)",
     CASE global_type WHEN 'unresolved' THEN 410 WHEN 'archive' THEN 410 WHEN 'redirect' THEN 301 END AS global_http_status,
     global_new_url AS "New URL for global 301",
