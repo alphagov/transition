1ba569088ccde3da1df109c8020af01d2805a2d9
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-26 16:24:33 +0000

    Convert FOI response script into a rake task
    This means that Postgres authentication will work as normal, and the
    task can be run from the root of the project.
    
    There are some mappings rows which cause encoding problems such as
    'Encoding::UndefinedConversionError: "\xE2" from ASCII-8BIT to UTF-8'.
    The paths do look somewhat mangled, so this catches the exception and
    excludes those rows for now. 616 rows in my development database are
    excluded by this.
    
    This still doesn't have any tests.

--- script/export_for_foi/export_hosts.sql
+++
@@ -1,9 +0,0 @@
-COPY (
-  SELECT
-    sites.abbr AS "Abbreviation",
-    hostname
-    FROM hosts
-    INNER JOIN sites ON hosts.site_id = sites.id
-    WHERE canonical_host_id IS NULL
-    ORDER BY sites.abbr, hostname
-) TO STDOUT WITH DELIMITER ',' CSV HEADER;
