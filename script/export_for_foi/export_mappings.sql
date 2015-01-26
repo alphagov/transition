a323778e2e677330bc1e31236992be53ccbd9aff
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-01-26 13:23:53 +0000

    Rearrange mappings columns into a more logical order

--- script/export_for_foi/export_mappings.sql
+++ script/export_for_foi/export_mappings.sql
@@ -2,10 +2,10 @@ COPY (
   SELECT
     sites.abbr AS "Abbreviation",
     path AS "Old Path",
+    CASE type WHEN 'unresolved' THEN 410 WHEN 'archive' THEN 410 WHEN 'redirect' THEN 301 END AS http_status,
     new_url AS "Redirect URL",
-    suggested_url AS "Suggested URL",
     archive_url AS "Custom Archive URL",
-    CASE type WHEN 'unresolved' THEN 410 WHEN 'archive' THEN 410 WHEN 'redirect' THEN 301 END AS http_status
+    suggested_url AS "Suggested URL"
     FROM mappings
     INNER JOIN sites ON mappings.site_id = sites.id
     WHERE sites.global_type IS NULL
