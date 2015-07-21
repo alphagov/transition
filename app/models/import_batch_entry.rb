4277427a565f902621ee1c4c22d3d471e67c3029
Michael Patrick <michael.patrick@digital.cabinet-office.gov.uk>
2015-07-21 14:50:39 +0100

    Scope for batch entries with custom archive URLs
    To aid presenting import stats to the user about archive mappings with custom
    archive URLs

--- app/models/import_batch_entry.rb
+++ app/models/import_batch_entry.rb
@@ -4,6 +4,8 @@ class ImportBatchEntry < MappingsBatchEntry
   scope :archives,   -> { with_type('archive') }
   scope :unresolved, -> { with_type('unresolved') }
 
+  scope :with_custom_archive_urls, -> { archives.where("archive_url IS NOT NULL") }
+
   def redirect?
     type == 'redirect'
   end
