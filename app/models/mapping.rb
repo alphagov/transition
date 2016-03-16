8f57abb4d509a9ee4b962ceee941013480ebb607
Murray Steele <muz@h-lame.com>
2016-03-16 12:03:56 +0000

    Fix failing specs caused by upgrade
    * Mapping.filtered_by_path & Mapping.filtered_by_new_url - where(true)
      is no longer a valid way to indicate a no-op scope when we provide a
      blank term to the scope.  Happily, returning nil from the scope is valid
      so we do that instead.
    * FilterHelper#hidden_filter_fields_except - the tag helpers no longer
      output attributes in the same order as they used to, instead of matching
      using specs we instead use the have_selector matcher to match on the dom
      directly regardless of whitespace, attribute ordering, etc..

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -53,10 +53,10 @@ class Mapping < ActiveRecord::Base
   scope :archives,  -> { with_type('archive') }
   scope :unresolved, ->{ with_type('unresolved') }
   scope :filtered_by_path, -> term do
-    where(term.blank? ? true : Mapping.arel_table[:path].matches("%#{term}%")).references(:mapping)
+    where(Mapping.arel_table[:path].matches("%#{term}%")).references(:mapping) if term.present?
   end
   scope :filtered_by_new_url, -> term do
-    where(term.blank? ? true : Mapping.arel_table[:new_url].matches("%#{term}%")).references(:mapping)
+    where(Mapping.arel_table[:new_url].matches("%#{term}%")).references(:mapping) if term.present?
   end
 
   def redirect?
--- spec/helpers/filter_helper_spec.rb
+++ spec/helpers/filter_helper_spec.rb
@@ -15,13 +15,13 @@ describe FilterHelper do
     it { is_expected.to be_an(ActiveSupport::SafeBuffer)}
 
     it 'includes links to all the fields except path_contains' do
-      expect(subject).to     include('<input id="type" name="type" type="hidden"')
-      expect(subject).not_to include('<input id="path_contains" name="path_contains" type="hidden"')
+      expect(subject).to     have_selector('input#type[name="type"][type="hidden"]')
+      expect(subject).not_to have_selector('input#path_contains[name="path_contains"][type="hidden"]')
     end
 
     it 'excludes fields that are blank' do
       allow(filter).to receive(:new_url_contains).and_return('')
-      expect(subject).not_to include('<input id="new_url_contains" name="new_url_contains" type="hidden"')
+      expect(subject).not_to have_selector('input#new_url_contains[name="new_url_contains"][type="hidden"]')
     end
   end
 end
