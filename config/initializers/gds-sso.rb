ef624d2d035e14bf9bcb51dea871e10ad3fa1588
Mikael <mikellallison@gmail.com>
2016-08-05 10:43:50 +0100

    Fetch Signon OAuth ID and secret from environment
    This change will allow transition to use auth tokens defined in hiera when it's deployed.
    
    This should eliminate the need to replace the file each time per environment.
    
    https://github.com/alphagov/govuk-puppet/commit/09e454056c1a4e50c1a23258fed4b796a84a3055
    
    https://github.gds/gds/deployment/pull/1056/commits/75b23782c508cf332de84fe7cf6e8d875e876fd7

--- config/initializers/gds-sso.rb
+++ config/initializers/gds-sso.rb
@@ -1,6 +1,6 @@
 GDS::SSO.config do |config|
   config.user_model   = 'User'
-  config.oauth_id     = 'oauth_id_defined_on_rollout'
-  config.oauth_secret = 'secret'
+  config.oauth_id     = ENV['SIGNON_OAUTH_ID']
+  config.oauth_secret = ENV['SIGNON_OAUTH_SECRET']
   config.oauth_root_url = Plek.current.find("signon")
 end
