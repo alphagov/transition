5a324a5d1e212f0707d453b03f6d50a2a46f760b
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2016-08-05 17:32:21 +0100

    Make the sites table filterable
    Several organisations have lots of sites now, which makes it harder to find
    the one you're looking for in the table. Making the table filterable, as we do
    for the organisations index, helps with this.

--- features/step_definitions/organisation_interaction_steps.rb
+++ features/step_definitions/organisation_interaction_steps.rb
@@ -1,3 +1,7 @@
 When(/^I filter organisations by "(.*?)"$/) do |text|
   fill_in 'Filter organisations', with: text
 end
+
+When(/^I filter sites by "(.*?)"$/) do |text|
+  fill_in 'Filter sites', with: text
+end
