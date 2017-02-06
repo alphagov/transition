ef4de65e276bc409eb6798eda2fbee93617b046e
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2017-02-06 10:28:39 +0000

    Suppress javascript errors in Poltergeist
    We've got intermittent-ish test failures around the charts js in Transition
    again, which get in the way of us making even small unrelated changes to the
    app. This time it's variations on this in the hits_summary feature.
    
    We're disabling raising of javascript errors in Poltergeist so that we can get
    builds running again.  We've got a [card for investigating the issue further][1]
    in our backlog.
    
    Trello story: https://trello.com/c/nvjibWTy/633-transition-test-failures
    
    [1]: https://trello.com/c/kaQn2aYB/528-transition-test-failures

--- features/support/poltergeist.rb
+++ features/support/poltergeist.rb
@@ -5,7 +5,8 @@ Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app, {
     phantomjs: Phantomjs.path,
     window_size: [1366, 768],
-    debug: false
+    debug: false,
+    js_errors: false,
   })
 end
 
