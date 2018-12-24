84d523fd4a22f843f3bb70ecb04323bdd4f4d5b8
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2018-12-24 12:05:47 +0000

    Delete downloaded whitehall mappings after importing
    These files are 30MB each, so downloading one twice a day and never deleting them is eating up disk space.
    
    Normally these would be removed as old release directories are deleted, but this app is not deployed very often.

--- spec/lib/transition/import/whitehall/mappings_spec.rb
+++ spec/lib/transition/import/whitehall/mappings_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 require 'transition/import/whitehall/mappings'
 
 describe Transition::Import::Whitehall::Mappings do
+  let(:tmpdir) { Rails.root + "tmp/" }
+
+  before do
+    tmpdir.mkdir unless tmpdir.exist?
+  end
+
   describe 'as_user' do
     subject { Transition::Import::Whitehall::Mappings.new(filename: 'foo').send(:as_user) }
 
@@ -16,6 +22,48 @@ describe Transition::Import::Whitehall::Mappings do
       describe '#is_robot' do
         subject { super().is_robot }
         it { is_expected.to be_truthy }
+      end
+    end
+  end
+
+  context "when downloading the file from whitehall" do
+    before do
+      stub_request(:get, "http://whitehall-admin.dev.gov.uk/government/mappings.csv")
+        .to_return(body: "some,mappings,csv")
+    end
+
+    it "deletes the downloaded file after processing" do
+      Timecop.freeze do
+        mappings = Transition::Import::Whitehall::Mappings.new(
+          username: "username",
+          password: "password",
+        )
+
+        expect(File.exist?(mappings.send(:default_filename))).to eq(false)
+        mappings.call
+        expect(File.exist?(mappings.send(:default_filename))).to eq(false)
+      end
+    end
+  end
+
+  context "when using a file off disk" do
+    let(:filename) { tmpdir + "test_filename.csv" }
+
+    before do
+      filename.write("some,mappings,csv")
+    end
+
+    after do
+      filename.delete
+    end
+
+    it "does not delete file after processing" do
+      Timecop.freeze do
+        mappings = Transition::Import::Whitehall::Mappings.new(filename: filename)
+
+        expect(File.exist?(filename)).to eq(true)
+        mappings.call
+        expect(File.exist?(filename)).to eq(true)
       end
     end
   end
