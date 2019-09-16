fccfe6bca4458e6f58dfcecba9b352a0ec5ff656
Tom Hipkin <tomhipkin@gmail.com>
2019-09-16 11:35:57 +0100

    (feature) Sidekiq task can ingest W3C log files from S3

--- lib/services.rb
+++ lib/services.rb
@@ -2,6 +2,6 @@ require 'aws-sdk-s3'
 
 module Services
   def self.s3
-    @s3 ||= Aws::S3::Client.new
+    @s3 ||= Aws::S3::Client.new(profile: ENV['AWS_PROFILE'], region: ENV['AWS_REGION'])
   end
 end
