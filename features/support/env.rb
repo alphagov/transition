8a6a1b7a59080b3d95b16c63ed6f9b03e11c5897
Issy Long <me@issyl0.co.uk>
2019-10-24 09:13:26 +0100

    Allow DatabaseCleaner to use a remote DATABASE_URL
    - In this app, we use DatabaseCleaner instead of Rails' transactional
      features.
    - In making this app work with Docker, obviously the database URL is not
      localhost anymore, so
      [DatabaseCleaner complains](https://github.com/DatabaseCleaner/database_cleaner/blob/v1.7.0/README.markdown#safeguards).
    - To get around this, enable remote database urls in tests. Later
      (unrealeased, yet documented in the default README!) versions of
      DatabaseCleaner will support a safelist of database URLs to be
      specified.

--- features/support/env.rb
+++ features/support/env.rb
@@ -31,6 +31,7 @@ ActionController::Base.allow_rescue = false
 # Remove/comment out the lines below if your app doesn't have a database.
 # For some databases (like MongoDB and CouchDB) you may need to use :truncation instead.
 begin
+  DatabaseCleaner.allow_remote_database_url = true
   DatabaseCleaner.strategy = :transaction
 rescue NameError
   raise "You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it."
