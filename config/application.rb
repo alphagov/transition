847d903eabf188b42f06bc942234df8331f8ca0c
Issy Long <me@issyl0.co.uk>
2020-03-09 13:31:03 +0000

    Run `rails app:update` and accept most of the defaults
    - This accepts most of the new default settings apart from ones we'd
      entirely edited (routes.rb) and settings we'd had for ages.
    - I didn't do these particularly scientifically, so this needs some
      review of the diff.
    - This leaves `config.file_watcher = ActiveSupport::EventedFileUpdateChecker`
      commented out as adding the `listen` gem that it requires and enabling
      auto-reloading in development is out of scope for this card.
    - I deleted the autogenerated `config/storage.yml` and
      `config/cable.yml` as we don't use ActiveStorage or ActionCable.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,30 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Transition
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    config.eager_load_paths += %W(#{config.root}/lib)
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = "Europe/London"
-
-    config.active_record.belongs_to_required_by_default = false
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # TODO: this is no longer an encouraged pattern for dynamic error pages
-    # as it has many edgecases (See: https://github.com/rails/rails/pull/17815)
-    # We should consider changing how we do this.
-    # Route exceptions to our custom error pages.
-    config.exceptions_app = self.routes
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,21 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
-
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
-
-ActiveSupport::Inflector.inflections do |inflect|
+ActiveSupport::Inflector.inflections(:en) do |inflect|
   # Careful with this - it changes all conventions around fields
   # like :archive_url or :http_status, meaning things like validators
   # have to be renamed, e.g. NonBlankUrlValidator -> NonBlankURLValidator
@@ -24,3 +9,8 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.acronym "URLs"
   inflect.uncountable "unresolved"
 end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
