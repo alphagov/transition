51d4c8429f5da2836ad19361c95844e39108a026
Kevin Dew <kevindew@me.com>
2022-04-29 20:05:29 +0100

    Remove scheduled Whitehall import
    At some point in time Whitehall stopped producing these mapping files
    and thus this task no longer has an affect.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,15 +3,6 @@ env :PATH, "/usr/local/bin:/usr/bin:/bin"
 set :output, error: "log/cron.error.log", standard: "log/cron.log"
 job_type :rake, "cd :path && /usr/local/bin/govuk_setenv transition bundle exec rake :task :output"
 
-# Run when the file has just been generated.
-# If it runs while the file is being generated, it will download an incomplete file!
-#
-# At the time of writing, the file is generated twice a day:
-# https://github.com/alphagov/whitehall/blob/master/config/schedule.rb#L18
-every :day, at: ["4:15am", "2pm"] do
-  rake "import:whitehall:mappings"
-end
-
 # every hour 7am-7pm, Mon-Fri
 every "0 07-19 * * 1-5" do
   rake "import:dns_details"
