1aff5f4865676ced8f03d01e6013f98ae8cefb08
Joseph Kempster <jkempster34@gmail.com>
2023-09-12 17:54:52 +0100

    Add "admin" layout and move current layout to "legacy"
    We want to use the GOV.UK Design System for new pages.
    
    This:
    - Adds a new layout for the GOV.UK Design System, "admin_layout"
    - Keeps the current layout as "application_layout", so that it the default, but
      we can selectively add the new layout.
    - Moves the JavaScript and CSS of the "application_layout" into "legacy"
      folders.
    - Updates the Jasmine config to include the legacy layout.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link application.css
+//= link legacy_layout.css
 //= link application.js
+//= link legacy_layout.js
 //= link html5.js
---
+++ app/assets/javascripts/legacy_layout.js
@@ -0,0 +1,4 @@
+//= require select2
+//= require mousetrap.min
+//= require URI.min
+//= require_tree ./legacy
