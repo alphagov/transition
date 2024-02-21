9b747235d4a99fedb6393a653693b33fd39da7e0
Joseph Kempster <jkempster34@gmail.com>
2024-02-21 12:14:43 +0000

    Fix error when a site doesn't have an abbr
    We removed the abbr attribute for new sites in #1473.
    
    This page raises an error when a new site without an abbr is present.
    
    Sort by hostname instead. All sites have a default host with a hostname.

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -8,6 +8,6 @@ class OrganisationsController < ApplicationController
     @organisation = Organisation.find_by!(whitehall_slug: params[:id])
     sites = @organisation.sites.includes(:hosts)
     extra_sites = @organisation.extra_sites.includes(:hosts)
-    @sites = (sites + extra_sites).sort_by(&:abbr)
+    @sites = (sites + extra_sites).sort_by { |site| site.default_host.hostname }
   end
 end
