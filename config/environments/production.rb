36637b070aa9c9d1df225600e70be7be648fa24e
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2024-08-12 12:07:15 +0100

    Delete unused Sass config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,10 +78,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Rather than use a CSS compressor, use the SASS style to perform compression
-  config.sass.style = :compressed
-  config.sass.line_comments = false
-
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
