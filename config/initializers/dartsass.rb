34a79a843fa1db74094bd9e10a94f349e6583baf
Bruce Bolt <bruce.bolt@dsit.gov.uk>
2026-03-19 08:33:40 +0000

    Change method for importing all components from govuk_publishing_components
    This has been changed, as per the upgrade guide [1].
    
    1: https://github.com/alphagov/govuk_publishing_components/pull/5188

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -3,7 +3,7 @@ APP_STYLESHEETS = {
   "legacy_layout.scss" => "legacy_layout.css",
 }.freeze
 
-all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.all_stylesheets)
+all_stylesheets = APP_STYLESHEETS.merge(GovukPublishingComponents::Config.component_guide_stylesheet)
 Rails.application.config.dartsass.builds = all_stylesheets
 
 Rails.application.config.dartsass.build_options << " --quiet-deps"
