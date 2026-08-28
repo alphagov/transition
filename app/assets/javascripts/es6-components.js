c355a0882358cb999e44d45108453f488d5695a3
callumknights <callum.knights@digital.cabinet-office.gov.uk>
2026-08-28 17:36:48 +0100

    Bump govuk_publishing_components to v68.3.0
    This commit is necessary as govuk_publishing_components v68.3.0 includes
    govuk-frontend v6.
    
    I've tried to keep this as lightweight as possible, as despite the component
    guide stating many more Js components than required here are needed, none of those
    changed with this release, and the app did fine without them before.
    So this commit merely:
    - Removes the `layout-header` es6 component, as it's no longer needed.

--- app/assets/javascripts/es6-components.js
+++ app/assets/javascripts/es6-components.js
@@ -10,6 +10,5 @@
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
 //= require govuk_publishing_components/components/error-summary
-//= require govuk_publishing_components/components/layout-header
 //= require govuk_publishing_components/components/radio
 //= require govuk_publishing_components/components/skip-link
